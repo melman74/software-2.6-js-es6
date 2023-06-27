e3ed20f6381c20e497fb2843e23b88768092e0ca
Jeff Melton <melman74@yahoo.com>
2023-06-27 13:00:57 -0500

    non-arrow Implementation

---
+++ index.js
@@ -0,0 +1,63 @@
+/*
+    You are to use arrow functions for this task.
+
+    Every second, change the background color of <span> according to the elements in `colours` array. The display of color should repeat from the top.
+
+    Hint:
+    - Look for interval setting functions.
+    - You should use DOM Manipulation to update the style of <div> element.
+
+    Code Challenge:
+    - Provide a checkbox "stop". When it is checked, the colour change will not repeat and end at the last element.
+*/
+
+
+const colours = [
+	{
+		color: "red",
+		value: "#f00"
+	},
+	{
+		color: "green",
+		value: "#0f0"
+	},
+	{
+		color: "blue",
+		value: "#00f"
+	},
+	{
+		color: "cyan",
+		value: "#0ff"
+	},
+	{
+		color: "magenta",
+		value: "#f0f"
+	},
+	{
+		color: "yellow",
+		value: "#ff0"
+	},
+	{
+		color: "black",
+		value: "#000"
+	}
+];
+
+let targetDiv = document.querySelector("#target");
+
+let colornum = 0;
+
+setInterval(changeColor, 1000);
+
+function changeColor() {
+    targetDiv.style.backgroundColor = colours[colornum].value;
+    colornum++;
+    if (colornum === colours.length) {
+        colornum = 0;
+    }
+}
+
+
+
+
+
